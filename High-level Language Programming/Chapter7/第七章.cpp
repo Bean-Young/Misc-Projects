86d1eec0e4f82f678835d74450c56f330fa0a8dd
YoungBean <yangyuezhe@gmail.com>
2024-10-26 14:07:23 +0800

    Add files via upload

---
+++ High-level Language Programming/Chapter7/第七章.cpp
@@ -0,0 +1,264 @@
+﻿//17.
+#include <stdio.h>
+void next(int n)
+{
+	int i;
+	i = n / 10;
+	if (i != 0) next(i);
+	putchar(n % 10 + '0');
+	putchar(32);
+}
+void main()
+{
+	int num;
+	scanf_s("%d",&num);
+	if (num < 0)
+	{
+		putchar('-');
+		putchar(' ');
+		num = -num;
+	}
+	next(num);
+	printf("\n");
+}
+//16.
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+char s[100];
+int change(char c)
+{
+	if ((c >='0') && (c <= '9')) return(c - 48); else return(c - 55);
+}
+int shi(int flag)
+{
+	if (flag == 0) return(change(s[flag]) * pow(16, (strlen(s) - 1)));
+	return(change(s[flag]) * pow(16, (strlen(s) - 1 - flag)) + shi(flag - 1));
+}
+void main()
+{
+	gets_s(s);
+	printf("%d\n", shi(strlen(s)-1));
+}
+//14.
+#include <stdio.h>
+#include <math.h>
+int n = 10, m = 5;
+int x;
+int y;
+void scan(float a[11][6])
+{
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+		{
+			scanf_s("%f", &a[i][j]);
+			a[0][j] += a[i][j];
+			a[i][0] += a[i][j];
+		}
+}
+void average(float a[11][6])
+{
+	for (int i = 1; i <= n; i++)
+		a[i][0] /= m;
+	for (int j = 1; j <= m; j++)
+		a[0][j] /= n;
+}
+void print(float a[11][6])
+{
+	for (int i = 1; i <= n; i++)
+		printf("%-7.2f", a[i][0]);
+	printf("\n");
+	for (int j = 1; j <= m; j++)
+		printf("%-7.2f", a[0][j]);
+	printf("\n");
+}
+void max(float a[11][6])
+{
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= m; j++)
+			int(a[x][y]) < int(a[i][j]) ? x = i, y = j : 0;
+	printf("%5d%5d%5d\n", x, y, int(a[x][y]));
+}
+void fangcha(float a[11][6])
+{
+	float sum = 0;
+	float fc = 0;
+	for (int i = 1; i <= n; i++)
+		sum += a[i][0];
+	for (int i = 1; i <= n; i++)
+		fc += (pow(a[i][0], 2) - pow(sum / n, 2));
+	printf("%6.2f\n", fc / n);
+}
+void main()
+{
+	float a[11][6] = { 0 };
+	scan(a);
+	average(a);
+	print(a);
+	x = 1;
+	y = 1;
+	max(a);
+	fangcha(a);
+}
+//13.
+#include <stdio.h>
+float pn(int n,int x)
+{
+	if (n == 0) return 1;
+	if (n == 1) return x;
+	return((2 * n - 1) * x - pn((n - 1), x) - (n - 1) * pn((n - 2), x)) / n;
+}
+void main()
+{
+	int n,x;
+	scanf_s("%d%d", &n,&x);
+	printf("%6.2f\n", pn(n, x));
+}
+
+
+//10.
+	//利用指针使函数返回多个值
+#include <stdio.h>
+void find_longest(char str[],int *b,int *e)
+{
+	int max = 0, begin = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+	if (!(((str[i]>='a')&&(str[i]<='z')) || ((str[i]>='A')&&(str[i]<='Z'))))
+	{
+		if (max < (i - begin))
+		{
+			max = i - begin;
+			*b = begin;
+			*e = i - 1;
+		}
+		begin = i + 1;
+	}
+}
+void main()
+{
+	char s[100];
+	int begin, end;
+	gets_s(s);
+	find_longest(s, &begin, &end);
+	for (int i = begin; i <= end; i++)
+		printf("%c", s[i]);
+	printf("\n");
+}
+//8.
+#include <stdio.h>
+void print(char s[])
+{
+	for (int i = 0; s[i] != '\0'; i++)
+	{
+		printf("%c", s[i]);
+		if (s[i + 1] == '\0') printf("\n"); else printf(" ");
+	}
+}
+void main()
+{
+	char str[100];
+	gets_s(str);
+	print(str);
+}
+//7.
+#include <stdio.h>
+void vowel(char str[], char str1[])
+{
+	int j = 0;
+	for (int i = 0; str[i] != '\0'; i++)
+		if ((str[i] == 'a') || (str[i] == 'e') || (str[i] == 'i') || (str[i] == 'o') || (str[i] == 'u') ||
+			(str[i] == 'A') || (str[i] == 'E') || (str[i] == 'I') || (str[i] == 'O') || (str[i] == 'U')) str1[j++] = str[i];
+}
+void main()
+{
+	char s[100], s1[100] = { '\0' };
+	gets_s(s);
+	vowel(s, s1);
+	printf("%s\n", s1);
+}
+//6.
+#include <stdio.h>
+void connect(char str1[], char str2[], char str[])
+{
+	int i, j;
+	for (i = 0; str1[i] != '\0'; i++)
+		str[i] = str1[i];
+	for (j = 0; str2[j] != '\0'; j++)
+		str[j + i] = str2[j];
+}
+void main()
+{
+	char s1[100], s2[100], s[100] = { '\0' };
+	gets_s(s1);
+	gets_s(s2);
+	connect(s1, s2, s);
+	printf("%s\n", s);
+}
+//5.
+	//extern 用于全局变量
+#include <string.h>
+#include <stdio.h>
+char str[100] = { '\0' };
+void jh(char* p, char* q)
+{
+	char t;
+	t = *p;
+	*p = *q;
+	*q = t;
+}
+void exchang_for_string()
+{
+	for ( int i = 0 ,j = strlen(str); i < j; i++, j--)
+		jh(&str[i], &str[j - 1]);
+}
+void main()
+{
+	gets_s(str);
+	exchang_for_string();
+	printf("%s\n", str);
+}
+//4.
+#include <stdio.h>
+void jh(int* p,int *q)
+{
+	int t;
+	t = *p;
+	*p = *q;
+	*q = t;
+}
+void exchang(int a[][3])
+{
+	for (int i = 0; i < 3; i++)
+		for (int j = i + 1; j<3; j++)
+			jh(&a[i][j], &a[j][i]);
+}
+void main()
+{
+	int a[3][3];
+	for (int i = 0; i < 3; i++)
+		for (int j = 0; j < 3; j++)
+			scanf_s("%d", &a[i][j]);
+	exchang(a);
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+			printf("%5d", a[i][j]);
+		printf("\n");
+	}
+}
+//3.
+#include <stdio.h>
+int prime(int n)
+{
+	int flag = 1;
+	for (int i = 2; i <= n / 2 && flag == 1; i++)
+		if (n % i == 0) flag = 0;
+	return(flag);
+}
+void main()
+{
+	int n;
+	scanf_s("%d", &n);
+	if (prime(n)) printf("Yes.\n"); else printf("No.\n");
+}
+*/
---
+++ High-level Language Programming/Chapter9/第九章.cpp
@@ -0,0 +1,294 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char* fun(char* s)
+{
+    int i, j, k, n; char* p, * t;
+    n = strlen(s) + 1;
+    t = (char*)malloc(n * sizeof(char));
+    p = (char*)malloc(n * sizeof(char));
+    j = 0;  k = 0;
+    for ( i = 0; i < n; i++)
+    { if (((s[i] >= 'a') && (s[i] <= 'z')) || ((s[i] >= 'A') && (s[i] <= 'Z')))
+      {
+        t[j] = s[i];  j++;
+    }
+else { p[k] = s[i]; k++; }
+    }
+    for (i = 0; i < j + k; i++)
+        t[j + i] = p[i];
+    t[j + k] ='\0';
+    return t;
+}
+void main()
+{
+    char s[80];
+    printf("please input : ");
+    scanf("%s", s);
+    printf("\nThe result is : %s\n", fun(s));
+}
+/*
+//12.
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define LEN sizeof(struct student)
+int n = 0;
+struct student {
+    int age;
+    long num;
+    char name[20];
+    unsigned char sex;
+    struct student* next;};
+struct student* creat()
+{                                                
+    struct student* head, * p1, * p2;
+    head = 0;
+    p1 = p2 = (struct student*)malloc(LEN);
+    printf("请输入年龄，学号，姓名，性别：\n");
+    scanf("%d %ld %s %c", &p1->age, &p1->num, p1->name, &p1->sex);
+    while (p1->num != 0) {
+        n++;
+        if (n == 1) head = p1;
+        else p2->next = p1;
+        p2 = p1;
+        p1 = (struct student*)malloc(LEN);
+        printf("请输入年龄，学号，姓名，性别：\n");
+        scanf("%d %ld %s %c", &p1->age, &p1->num, p1->name, &p1->sex);
+
+    }
+    p2->next = 0;
+    return head;
+}
+struct student* deldate(struct student* p) 
+{                             
+    int dnum;
+    printf("请输入要删除的学生的年龄:\n");
+    scanf("%d", &dnum);
+    struct student* head, * p1, * p2;
+    head = p1 = p2 = p;
+    while (p1 != 0)
+    {
+        if (p1->age == dnum)
+        {
+            if (head == p1) head = p1->next;
+            else p2->next = p1->next;
+            p1 = p1->next;
+        }
+        else 
+        {
+            p2 = p1;
+            p1 = p1->next;
+        }
+    }
+    return head;
+}
+void print(struct student* p1)
+{                                    
+    while (p1 != 0)
+    {
+        printf("%d %ld %s %c\n", p1->age, p1->num, p1->name, p1->sex);
+        p1 = p1->next;
+    }
+}
+void main()
+{
+    struct student* head = creat();
+    struct student* head1 = deldate(head);
+    print(head1);
+}
+/*
+//6.
+#include <stdio.h>
+struct person {
+    int num;
+    struct person* next;
+}per[13];
+void main()
+{
+    struct person* p1 = &per[0];
+    struct person* p2 = &per[0];
+    int i, j = 1;
+
+    for (i = 0; i < 13; i++) 
+    {
+        per[i].num = i;
+        if (i != 12) per[i].next = &per[i + 1];
+        else per[i].next = &per[0];
+    }
+
+    while (i > 1)
+    {
+        if (j == 3)
+        {
+            j = 1;
+            p2->next = p1->next;
+            p1 = p1->next;
+            i--;
+
+        }
+        else {
+            p2 = p1;
+            p1 = p1->next;
+            j++;
+        }
+    }
+    printf("最后剩下第%d位 ", p2->num + 1);
+}
+//5.
+#include<stdio.h>
+struct student {
+    int num;
+    char name[8];
+    int score[3];
+}s[10];
+void input(struct student s[])
+{
+    int i, j;
+    for (i = 0; i < 10; i++)
+    {
+        printf("请输入第%d学生的学号:\n", i + 1);
+        scanf_s("%d", &s[i].num);
+        printf("请输入第%d学生的姓名：\n", i + 1);
+        scanf_s("%s", s[i].name,8);
+        printf("请输入第%d学生的三个成绩：\n", i + 1);
+        for (j = 0; j < 3; j++)
+            scanf_s("%d", &s[i].score[j]);
+    }
+}
+void print(struct student s[])
+{
+    int i, j;
+    float sum[3] = { 0 };
+    printf("3门课的总平均成绩：\n");
+    for (j = 0; j < 3; j++)
+    {
+        for (i = 0; i < 5; i++)
+            sum[j] += s[i].score[j];
+        printf("%8.2f\n", sum[j] / 4);
+    }
+    float max = s[0].score[0];
+    int m=0, n=0;
+    for (i = 0; i < 5; i++)
+    {
+        for (j = 0; j < 3; j++)
+            if (max < s[i].score[j])
+            {
+                max = s[i].score[j];
+                m = i; n = j;
+            }
+    }
+    float aver = 0;
+    for (j = 0; j < 3; j++)
+        aver += s[m].score[j];
+    printf("最高分为:%2.2f\n", max);
+    printf("最高分的学生的数据为：\n");
+    printf("%4d %4s %4.2d %4.2d %4.2d %4.2f\n", s[m].num, s[m].name, s[m].score[0], s[m].score[1], s[m].score[2], aver / 3);
+}
+void main()
+{
+    printf("请输入学号，姓名，三个成绩：\n");
+    input(s);
+    print(s);
+}
+//4.
+#include<stdio.h>
+struct student {
+    int num;
+    char name[8];
+    int score[3];
+}s[5];
+void input(struct student s[])
+{
+    int i, j;
+    for (i = 0; i < 5; i++)
+    {
+        printf("请输入第%d学生的学号:\n", i + 1);
+        scanf_s("%d", &s[i].num);
+        printf("请输入第%d学生的姓名：\n", i + 1);
+        scanf_s("%s", s[i].name,8);
+        printf("请输入第%d学生的三个成绩：\n", i + 1);
+        for (j = 0; j < 3; j++)
+            scanf_s("%d", &s[i].score[j]);
+    }
+}
+void print(struct student s[])
+{
+    int i, j;
+    for (i = 0; i < 5; i++)
+    {
+        printf("%5d%10s\t", s[i].num, s[i].name);
+        for (j = 0; j < 3; j++)
+            printf("%d\t", s[i].score[j]);
+        printf("\n");
+    }
+}
+void main()
+{
+    printf("请输入学号，姓名，三个成绩：\n");
+    input(s);
+    print(s);
+}
+//3.
+#include<stdio.h>
+struct student {
+    int num;
+    char name[8] = { '\0' };
+    int score[3];
+}s[5];
+void print(struct student s[])
+{
+    int i, j;
+    for (i = 0; i < 5; i++)
+    {
+        printf("%5d%10s\t", s[i].num, s[i].name);
+        for (j = 0; j < 3; j++)
+            printf("%d\t", s[i].score[j]);
+        printf("\n");
+    }
+}
+void main()
+{
+    printf("请输入学号，姓名，三个成绩：\n");
+    int i, j;
+    for (i = 0; i < 5; i++)
+    {
+        printf("请输入第%d学生的学号:\n", i + 1);
+        scanf_s("%d", &s[i].num);
+        printf("请输入第%d学生的姓名：\n", i + 1);
+        scanf_s("%s",s[i].name,8);
+        printf("请输入第%d学生的三个成绩：\n", i + 1);
+        for (j = 0; j < 3; j++)
+            scanf_s("%d", &s[i].score[j]);
+    }
+    print(s);
+}
+//1(2).
+#include<stdio.h>
+int data(struct days d);
+struct days {
+    int year;
+    int month;
+    int day;
+}d;
+int main()
+{
+    int n;
+    printf("请输入年，月，日:\n");
+    scanf_s("%d%d%d", &d.year, &d.month, &d.day);
+    n = data(d);
+    printf("%d年%d月%d日是%d年的第%d天\n", d.year, d.month, d.day, d.year, n);
+}
+int data(struct days d)
+{
+    int smonth[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+    int i, sum = 0;
+    for (i = 0; i < d.month; i++)
+        sum += smonth[i];
+    sum += d.day;
+    if (d.month > 2 && ((d.year % 4 == 0 && d.year % 100 != 0) || d.year % 400 == 0))
+        sum++;
+    return sum;
+}
+*/
