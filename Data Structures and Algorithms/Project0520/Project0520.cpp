251b1aa9e53e01863d5d0639be92aece082d2644
YoungBean <yangyuezhe@gmail.com>
2024-10-26 14:14:20 +0800

    Add files via upload

---
+++ Data Structures and Algorithms/Project0520/Project0520.cpp
@@ -0,0 +1,158 @@
+﻿#include <iostream>
+using namespace std;
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+#define MAXSIZE 100
+typedef int Status;
+typedef int ElemType;
+typedef struct
+{
+    ElemType* elem;
+    int length;
+} SqList;
+typedef struct LNode
+{
+    ElemType data;
+    struct LNode* next;
+}LNode, * LinkList;
+Status Init_SqList(SqList& L)
+{
+    L.elem = new ElemType[MAXSIZE];
+    if (!L.elem) exit(OVERFLOW);
+    L.length = 0;
+    return OK;
+}
+Status Insert_SqList(SqList& L, int i, ElemType e)
+{
+    if (i<1 || i>L.length+1) return ERROR;
+    if (L.length == MAXSIZE) return OVERFLOW;
+    for (int j = L.length - 1; j >= i - 1; j--)
+        L.elem[j + 1] = L.elem[j];
+    L.elem[i - 1] = e;
+    L.length++;
+    return OK;
+}
+Status Delete_SqList(SqList& L, int i, ElemType& e)
+{
+    if (i<1 || i>L.length) return ERROR;
+    for (int j = i; j <= L.length - 1; j++)
+        L.elem[j - 1] = L.elem[j];
+    L.length--;
+    return OK;
+}
+Status Init_LinkList(LinkList& L)
+{
+    L = new LNode;
+    if (!L) return ERROR;
+    L->next = NULL;
+    return OK;
+}
+void CreateLinkList(LinkList& L, int n)
+{
+    L = new LNode;
+    L->next = NULL;
+    LNode* r = L;
+    for (int i = 0; i < n; i++)
+    {
+        LNode* p = new LNode;
+        cin >> p->data;
+        p->next = NULL;
+        r->next = p;
+        r = p;
+    }
+}
+Status Print_LinkList(LinkList& L)
+{
+    LNode* p = L->next;
+    while (p)
+    {
+        cout << p->data << "\t";
+        p = p->next;
+    }
+    cout << endl;
+    return OK;
+}
+Status Print_SqList(SqList L)
+{
+    for (int i = 1; i <= L.length; i++)
+        cout << L.elem[i - 1]<<"\t";
+    cout << endl;
+    return OK;
+}
+Status Insert_LinkList(LinkList L, int i, ElemType e)
+{
+    LNode* p = L;
+    int j = 0;
+    while (p && (j < i - 1))
+    {
+        p = p->next;
+        j++;
+    }
+    if (!p || j > i - 1) return ERROR;
+    LNode* s = new LNode;
+    s->data = e;
+    s->next = p->next;
+    p->next = s;
+    return OK;
+}
+Status CreateSqList(SqList &L,int n)
+{
+    for (int i=0; i < n; i++)
+    {
+        cin >> L.elem[i];
+        L.length++;
+    }
+    return OK;
+}
+Status Delete_LinkList(LinkList L, int i)
+{
+    LNode* p = L;
+    int j = 0;
+    while (p && (j < i - 1))
+    {
+        p = p->next;
+        j++;
+    }
+    if (!p || j > i - 1) return ERROR;
+    LNode* q = p->next;
+    p->next = q->next;
+    delete q;
+    return OK;
+}
+Status Overturn(LinkList L)
+{
+    LinkList p = L->next;
+    L->next = NULL;
+    while (p)
+    {
+        LNode* r = p;
+        p = p->next;
+        r->next = L->next;
+        L->next = r;
+    }
+    return OK;
+}
+int main()
+{
+    int n,num;
+    SqList Sq;
+    LinkList Link;
+    Init_SqList(Sq);
+    CreateSqList(Sq,5);
+    Print_SqList(Sq);
+    Insert_SqList(Sq, 3, 10);
+    Print_SqList(Sq);
+    Delete_SqList(Sq, 3, num);
+    Print_SqList(Sq);
+    Init_LinkList(Link);
+    CreateLinkList(Link, 5);
+    Print_LinkList(Link);
+    Insert_LinkList(Link, 3, 10);
+    Print_LinkList(Link);
+    Overturn(Link);
+    Print_LinkList(Link);
+    Delete_LinkList(Link, 4);
+    Print_LinkList(Link);
+    return 0;
+}
---
+++ Data Structures and Algorithms/Project0527/Project0527.cpp
@@ -0,0 +1,226 @@
+﻿#include <iostream>
+using namespace std;
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+#define MAXSIZE 100
+#define MAXQSIZE 100
+typedef int Status;
+typedef int ElemType;
+typedef int SElemType;
+typedef int QElemType;
+typedef struct
+{
+	SElemType* base;
+	SElemType* top;
+	int stacksize;
+}SqStack;
+typedef struct StackNode
+{
+	ElemType data;
+	struct StackNode* next;
+}StackNode,*LinkStack;
+typedef struct QNode
+{
+	QElemType data;
+	struct QNode* next;
+}QNode,*QueuePtr;
+typedef struct
+{
+	QueuePtr front;
+	QueuePtr rear;
+}LinkQueue;
+Status Init_SqStack(SqStack &S)
+{
+	S.base = new SElemType[MAXSIZE];
+	if (!S.base) return(OVERFLOW);
+	S.top = S.base;
+	S.stacksize = MAXSIZE;
+	return OK;
+}
+Status Push_SqStack(SqStack *S, SElemType e)
+{
+	if (S->top - S->base == MAXSIZE) return ERROR;
+	*(S->top++) = e;
+	return OK;
+}
+Status Pop_SqStack(SqStack* S,SElemType &e)
+{
+	if (S->top == S->base) return ERROR;
+	e = *--S->top;
+	return OK;
+}
+Status GetTop_SqStack(SqStack* S)
+{
+	if (S->top == S->base) return ERROR;
+	return *(S->top - 1);
+}
+Status Init_LinkStack(LinkStack& S)
+{
+	S = NULL;
+	return OK;
+}
+Status Push_LinkStack(LinkStack& S, SElemType e)
+{
+	StackNode* p = new StackNode;
+	p->data = e;
+	p->next = S;
+	S = p;
+	return OK;
+}
+Status Pop_LinkStack(LinkStack & S, SElemType& e)
+{
+	if (S==NULL) return ERROR;
+	e = S->data;
+	StackNode* p = S;
+	S = S->next;
+	delete p;
+	return OK;
+}
+SElemType GetTop_LinkStack(LinkStack S)
+{
+	if (S != NULL) return S->data;
+}
+void solve_SqStack(SqStack& S)
+{
+	for (int i = 1; i <= 5; i++)
+	{
+		int n;
+		cin >> n;
+		Push_SqStack(&S, n);
+	}
+}
+void solve_Sq_to_Link(SqStack& S, LinkStack& L)
+{
+	int e;
+	while (Pop_SqStack(&S,e)) Push_LinkStack(L, e);
+}
+void solve_LinkStack(LinkStack& L)
+{
+	int e;
+	while (Pop_LinkStack(L, e)) cout << e << "\t";
+	cout << endl;
+}
+typedef struct
+{
+	QElemType* base;
+	int front;
+	int rear;
+}SqQueue;
+Status Init_SqQueue(SqQueue& Q)
+{
+	Q.base = new QElemType[MAXQSIZE];
+	if (!Q.base) return(OVERFLOW);
+	Q.front = Q.rear = 0;
+	return OK;
+}
+Status En_SqQueue(SqQueue& Q, QElemType e)
+{
+	if ((Q.rear + 1) % MAXQSIZE == Q.front) return ERROR;
+	Q.base[Q.rear] = e;
+	Q.rear = (Q.rear + 1) % MAXQSIZE;
+	return OK;
+}
+Status De_SqQueue(SqQueue& Q, QElemType& e)
+{
+	if (Q.front == Q.rear) return ERROR;
+	e = Q.base[Q.front];
+	Q.front = (Q.front + 1) % MAXQSIZE;
+	return OK;
+}
+Status Init_LinkQueue(LinkQueue &Q)
+{
+	Q.front = Q.rear = new QNode;
+	Q.front->next = NULL;
+	return OK;
+}
+Status En_LinkQueue(LinkQueue& Q, QElemType e)
+{
+	QNode* p = new QNode;
+	p->data = e;
+	p->next = NULL;
+	Q.rear->next = p;
+	Q.rear = p;
+	return OK;
+}
+Status De_LinkQueue(LinkQueue& Q, QElemType& e)
+{
+	if (Q.front == Q.rear) return ERROR;
+	QNode* p = Q.front->next;
+	e = p->data;
+	Q.front->next = p->next;
+	if (Q.front->next==NULL) Q.rear = Q.front;
+	delete p;
+	return OK;
+}
+void solve_SqQueue(SqQueue &Q)
+{
+	int n;
+	for (int i = 1; i <= 5; i++)
+	{
+		cin >> n;
+		En_SqQueue(Q, n);
+	}
+}
+void solve_Sq_to_Link_Q(SqQueue& Q, LinkQueue& L)
+{
+	int e;
+	for (int i = 1; i <= 2; i++)
+	{
+		De_SqQueue(Q, e);
+		En_LinkQueue(L, e);
+	}
+}
+void solve_LinkQueue(LinkQueue& L)
+{
+	int e;
+	for (int i = 1; i <= 2; i++)
+	{
+		De_LinkQueue(L, e);
+		cout << e << "\t";
+	}
+	cout << endl;
+}
+bool Matching()
+{
+	LinkStack S;
+	Init_LinkStack(S);
+	int flag = 1;
+	int flag_c;
+	char ch;
+	cin >> ch;
+	while (ch != '#' && flag)
+	{
+		if (ch == '[')
+		{
+			flag_c = 1;
+			Push_LinkStack(S, flag_c);
+		}
+		if (ch == ']')
+		{
+			if (Pop_LinkStack(S, flag_c)) void;  else flag = 0;
+		}
+		cin >> ch;
+	}
+	if ((S == NULL) && flag) return true; else return false;
+}
+int main()
+{
+	SqStack Sq;
+	LinkStack Link;
+	SqQueue Q;
+	LinkQueue LinkQ;
+	Init_SqStack(Sq);
+	Init_LinkStack(Link);
+	solve_SqStack(Sq);
+	solve_Sq_to_Link(Sq, Link);
+	cout<<GetTop_LinkStack(Link)<<endl;
+	solve_LinkStack(Link);
+	Init_SqQueue(Q);
+	Init_LinkQueue(LinkQ);
+	solve_SqQueue(Q);
+	solve_Sq_to_Link_Q(Q, LinkQ);
+	solve_LinkQueue(LinkQ);
+	if (Matching()) cout << "Yes"; else cout << "No";
+	return 0;
+}
